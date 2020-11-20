3558e909460aa66a5b5a56421365c0391287311d
Bridie Bradley <bridie@webscope.co.nz>
2020-11-20 15:47:22 +1300

    Styling switches for accessibility.

--- src/pages/Options.vue
+++ src/pages/Options.vue
@@ -11,14 +11,34 @@
     <div class="list-group list-group-flush">
       <div class="list-group-item">
         <div class="custom-control custom-switch">
-          <label class="custom-control-label" for="customSwitch1">Show sign language videos</label>
           <input type="checkbox" class="custom-control-input" id="customSwitch1" />
+          <label class="custom-control-label" for="customSwitch1"><span>Show sign language videos</span></label>
         </div>
       </div>
-    </div>
-    <div class="custom-control custom-switch">
-      <input type="checkbox" class="custom-control-input" id="customSwitch1" />
-      <label class="custom-control-label" for="customSwitch1">Toggle this switch element</label>
+      <div class="list-group-item">
+        <div class="custom-control custom-switch">
+          <input type="checkbox" class="custom-control-input" id="customSwitch2" />
+          <label class="custom-control-label" for="customSwitch2"><span>Toggle this switch element</span></label>
+        </div>
+      </div>
+      <div class="list-group-item">
+        <div class="custom-control custom-switch">
+          <input type="checkbox" class="custom-control-input" id="customSwitch3" />
+          <label class="custom-control-label" for="customSwitch3"><span>Toggle this switch element</span></label>
+        </div>
+      </div>
+      <div class="list-group-item">
+        <div class="custom-control custom-switch">
+          <input type="checkbox" class="custom-control-input" id="customSwitch4" />
+          <label class="custom-control-label" for="customSwitch4"><span>Toggle this switch element</span></label>
+        </div>
+      </div>
+      <div class="list-group-item">
+        <div class="custom-control custom-switch">
+          <input type="checkbox" class="custom-control-input" disabled id="customSwitch5" />
+          <label class="custom-control-label" for="customSwitch5"><span>Toggle this switch element</span></label>
+        </div>
+      </div>
     </div>
   </Layout>
 </template>
@@ -30,3 +50,45 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  .list-group-item {
+    margin-bottom: 0.5rem;
+    padding: 0;
+    border: none;
+  }
+  .custom-control {
+    padding-left: 0;
+  }
+  .custom-control-label {
+    width: 100%;
+    line-height: 1.2rem;
+    padding: 0.75rem 4rem 0.75rem  1.25rem;
+    border: 1px solid #adb5bd;
+    border-radius: 0.5rem;
+    cursor: pointer;
+  }
+  .custom-control-input {
+    left: unset;
+    right: 0;
+  }
+  .custom-control-input:focus ~ .custom-control-label {
+    box-shadow: 0 0 0 0.2rem rgba(241, 89, 42, 0.25);
+  }
+  .custom-control-input:checked ~ .custom-control-label {
+    border-color: #f1592a;
+  }
+  .custom-control-input:not(:checked) ~ .custom-control-label {
+    border-color:#adb5bd;
+  }
+  .custom-switch .custom-control-label::before {
+    left: unset;
+    right: 1.5rem;
+    top: 1rem;
+  }
+  .custom-switch .custom-control-label::after {
+    left: unset;
+    right: calc(2.25rem + 2px);
+    top: calc(1rem + 2px);
+  }
+</style>
